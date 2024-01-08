e88fc9e56bd51d7d6de437f24475e36d6adb7701
ChangMinPark2 <98qkrckdals@naver.com>
2024-01-08 11:07:17 +0900

    feat(JavaScript): var,let,const

---
+++ 2.javascript-codes/var,let,const/scrips.js
@@ -0,0 +1,22 @@
+// var greeting = 'hello';
+// console.log(greeting);
+//
+// var greeting = 'hi'
+// console.log(greeting);
+//
+// greeting = 'how';
+// console.log(greeting);
+
+// // 중복 선언 불가, 재할당은 가능
+// let greeting = 'hello';
+// console.log(greeting);
+//
+// greeting = 'a';
+// console.log(greeting);
+
+
+// 중복 선언 X, 재할당  X;
+const greeting = 'hello';
+console.log(greeting);
+
+
