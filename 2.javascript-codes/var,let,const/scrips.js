3af37e66f869ba2a7cf1e3a9163e39354c40465b
ChangMinPark2 <98qkrckdals@naver.com>
2024-01-08 11:16:20 +0900

    feat(JavaScript): 참조 범위 알기

--- 2.javascript-codes/var,let,const/scrips.js
+++ 2.javascript-codes/var,let,const/scrips.js
@@ -15,8 +15,29 @@
 // console.log(greeting);
 
 
-// 중복 선언 X, 재할당  X;
-const greeting = 'hello';
-console.log(greeting);
+// // 중복 선언 X, 재할당  X;
+// const greeting = 'hello';
+// console.log(greeting);
+
+//var 은 하tn fpqpf tmzhvm
+// function func() {
+//     if (true) {
+//         var a = 'a';
+//         console.log(a);
+//     }
+//     console.log(a);
+// }
+// func();
+
+//let, const -> block 레벨 스코프
+function func() {
+    if (true) {
+        let a = 'a';
+        console.log(a);
+    }
+    console.log(a); // 안나옴 (블록 레벨이라서)
+}
+
+func()
 
 
