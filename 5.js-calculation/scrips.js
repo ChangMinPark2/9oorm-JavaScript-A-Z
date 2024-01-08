ac5b3251479e216fb464d3a89285991a1eab90e0
ChangMinPark2 <98qkrckdals@naver.com>
2024-01-08 12:36:06 +0900

    feat(JavaScript): 연산, Math - 실습

---
+++ 5.js-calculation/scrips.js
@@ -0,0 +1,18 @@
+const num1 = 20;
+const num2 = 10;
+
+let val;
+//간단 연산
+// val = num1 + num2;
+// val = num1 * num2;
+// val = num1 - num2;
+// val = num1 / num2;
+// val = num1 % num2;
+
+val = Math.E;
+val = Math.round(2.4);
+val = Math.ceil(2.4);
+val = Math.random();
+val = Math.floor(Math.random() * 20 + 1);
+
+console.log(val);
