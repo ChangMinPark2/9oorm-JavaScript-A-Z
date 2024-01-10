ed82d13cbd3727f01d8419ab4a6e4cfebb9646b8
ChangMinPark2 <98qkrckdals@naver.com>
2024-01-10 15:40:40 +0900

    feat(JavaScript): 상속 - 실습

--- classes/script.js
+++ classes/script.js
@@ -1,18 +1,41 @@
-class Person{
-    constructor(name, email, birthday) {
+// class Person{
+//     constructor(name, email, birthday) {
+//         this.name = name;
+//         this.email = email;
+//         this.birthday = birthday;
+//     }
+//     introduce() {
+//         return `Hello my name is ${this.name}`;
+//     }
+//
+//     static multipleNumbers(x, y) {
+//         return x * y;
+//     }
+// }
+// const john = new Person('john', 'aa@ba.com', '10-3-87');
+// console.log(john);
+// console.log(john.introduce());
+// console.log(Person.multipleNumbers(2, 7))
+
+class Person {
+    constructor(name, email) {
         this.name = name;
         this.email = email;
-        this.birthday = birthday;
-    }
-    introduce() {
-        return `Hello my name is ${this.name}`;
     }
 
-    static multipleNumbers(x, y) {
-        return x * y;
+    introduce() {
+        return `hello i am ${this.name}`;
     }
 }
-const john = new Person('john', 'aa@ba.com', '10-3-87');
-console.log(john);
+
+class Client extends Person {
+    constructor(name, email, phone, address) {
+        super(name, email);
+
+        this.phone = phone;
+        this.address = address;
+    }
+}
+
+const john = new Client('john', '.com', '010-1111', 'seoul');
 console.log(john.introduce());
-console.log(Person.multipleNumbers(2, 7));
